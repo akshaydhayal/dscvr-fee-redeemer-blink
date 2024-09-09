12223ee20c898b2f427cb73d7ded3d5cfdead03c
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-09-09 15:08:53 +0530

    init

---
+++ web/app/api/action/helper.ts
@@ -0,0 +1,21 @@
+import * as token from "@solana/spl-token";
+import { Connection, PublicKey } from "@solana/web3.js";
+
+export async function closeEmptyAccounts(connection:Connection,userPub:PublicKey,programId:PublicKey){
+    
+    const allAccounts=await connection.getTokenAccountsByOwner(userPub,{
+        programId:programId
+    });
+    const emptyAccounts=[];
+    const empty=allAccounts.value.forEach((acc)=>{
+        const amount=acc.account.data.readBigUInt64LE(64)
+        if(!amount){
+            emptyAccounts.push(acc.pubkey)
+        }
+    })
+    console.log("found "+allAccounts.value.length+" total accounts ");
+    console.log("found "+emptyAccounts.length+" empty accounts ");
+    // console.log("allAccounts : ,", allAccounts);
+    // console.log("empty : ,", emptyAccounts);
+    return emptyAccounts;
+}
---
+++ web/app/api/action/route.ts
@@ -0,0 +1,56 @@
+import { ActionGetResponse, ActionPostResponse, ACTIONS_CORS_HEADERS } from "@solana/actions";
+import { clusterApiUrl, Connection, PublicKey, Transaction } from "@solana/web3.js";
+import { closeEmptyAccounts } from "./helper";
+import { createCloseAccountInstruction, TOKEN_2022_PROGRAM_ID, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+
+export async function GET(request: Request) {
+  const response: ActionGetResponse = {
+    title: 'Fee Redeemer',
+    icon: 'https://s3.coinmarketcap.com/static-gravity/image/5cc0b99a8dd84fbfa4e150d84b5531f2.png',
+    description: 'Recover Accounts rent fees by closing the Empty token accounts',
+    label:"BLINK",
+    links:{
+      actions:[{
+        label: 'Close Token Program Accounts',
+        href:request.url+"?pg=token"
+      },
+    {
+      label:"Close Token22 Program Accounts",
+      href:request.url+"?pg=token22"
+    }]
+    }
+  };
+  return Response.json(response,{headers:ACTIONS_CORS_HEADERS});
+}
+
+export const OPTIONS=GET;
+
+export async function POST(request:Request){
+  const connection=new Connection(clusterApiUrl("devnet"));
+  const reqBody=await request.json();
+  let sender=new PublicKey(reqBody.account);
+  
+  const pg=new URL(request.url).searchParams.get("pg");
+  const programId=(pg=='token')?TOKEN_PROGRAM_ID:TOKEN_2022_PROGRAM_ID;
+  const emptyTAs=await closeEmptyAccounts(connection,sender,programId);
+
+  const tx=new Transaction();
+  tx.feePayer=sender;
+  const blockHeight=await connection.getLatestBlockhash();
+  tx.recentBlockhash=blockHeight.blockhash;
+  tx.lastValidBlockHeight=blockHeight.lastValidBlockHeight;
+  const ixs=emptyTAs.map((acc)=>createCloseAccountInstruction(acc,sender,sender,undefined,programId));
+  
+  if(ixs.length>0){
+    tx.add(...ixs);
+  }
+  const serialisedTx=tx.serialize({requireAllSignatures:false,verifySignatures:false}).toString("base64");
+  
+
+  const response:ActionPostResponse={
+    transaction:serialisedTx,
+    // transaction:"",
+    message:"Hi from "+sender.toBase58()
+  }
+  return Response.json(response,{headers:ACTIONS_CORS_HEADERS});
+}

d33bd0e4f5797056255a40217fb998d58260dad9
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-09-09 20:30:49 +0530

    Addde dscvr meta tags and csp

--- web/next.config.js
+++ web/next.config.js
@@ -1,30 +1,57 @@
-//@ts-check
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
+// export default nextConfig;
 
-/**
- * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
- **/
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.externals = [
-      ...(config.externals || []),
-      'bigint',
-      'node-gyp-build',
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'], // Add or remove extensions as needed
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "connect-src 'self' https://web3quests.vercel.app https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com https://api.devnet.solana.com wss://api.devnet.solana.com/; style-src 'self' https://fonts.googleapis.com ; img-src 'self'  blob: data:;",
+          },
+        ],
+      },
     ];
-    return config;
-  },
-  nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
   },
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
+export default nextConfig;
 
-module.exports = composePlugins(...plugins)(nextConfig);
+
+// //@ts-check
+
+// // eslint-disable-next-line @typescript-eslint/no-var-requires
+// const { composePlugins, withNx } = require('@nx/next');
+
+// /**
+//  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+//  **/
+// const nextConfig = {
+//   webpack: (config) => {
+//     config.externals = [
+//       ...(config.externals || []),
+//       'bigint',
+//       'node-gyp-build',
+//     ];
+//     return config;
+//   },
+//   nx: {
+//     // Set this to true if you would like to use SVGR
+//     // See: https://github.com/gregberge/svgr
+//     svgr: false,
+//   },
+// };
+
+// const plugins = [
+//   // Add more Next.js plugins to this list if needed.
+//   withNx,
+// ];
+
+// module.exports = composePlugins(...plugins)(nextConfig);
